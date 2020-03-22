613e3a6b4138e25efcbc8145befa5789ad18b0f4
todoratanasov <t.atanasov.atanasov@gmail.com>
2020-03-22 11:32:57 +0200

    edit models

--- back-end/models/poker/PokerLeague.js
+++ back-end/models/poker/PokerLeague.js
@@ -4,6 +4,7 @@ const leagueSchema = new mongoose.Schema({
   name: { type: mongoose.Schema.Types.String, unique: true },
   creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   creationDate: { type: mongoose.Schema.Types.Date, default: Date.now },
+  closeDate: { type: mongoose.Schema.Types.Date },
   participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   awaitingParticipants: [{ type: mongoose.Schema.Types.ObjectId }],
   isActive: { type: mongoose.Schema.Types.Boolean, default: "true" },
