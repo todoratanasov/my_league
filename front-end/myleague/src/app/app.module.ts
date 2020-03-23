b9f6f9a5342d79a7f6bb0513c9a24ad705f5646e
todoratanasov <t.atanasov.atanasov@gmail.com>
2020-03-23 15:21:48 +0200

    first wave of components created

--- front-end/myleague/src/app/app.module.ts
+++ front-end/myleague/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { MainNavigationComponent } from './header/main-navigation/main-navigation.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, MainNavigationComponent, SideBarComponent, HomeComponent, RegisterComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
